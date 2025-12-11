af4fb1a07c6ac1378c59f7acbfce5644ba1c1260
nayanrdeveloper <nayanrdeveloper@gmail.com>
2025-12-11 18:58:56 +0530

    feat: Initialize Next.js frontend project with core dependencies, layout, and configuration.

--- frontend-web/src/app/layout.tsx
+++ frontend-web/src/app/layout.tsx
@@ -1,18 +1,10 @@
 import { Toaster } from '@/components/ui/toaster';
+import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Playfair_Display } from 'next/font/google';
+import { Playfair_Display } from 'next/font/google';
 import './globals.css';
 import ReduxProvider from './providers';
-
-const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
-});
 
 const playfair = Playfair_Display({
     variable: '--font-playfair-display',
@@ -35,7 +27,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable} antialiased`}>
+            <body className={`${GeistSans.variable} ${GeistMono.variable} ${playfair.variable} antialiased`}>
                 <ReduxProvider>
                     {children}
                     <Toaster />
